f57c9e5fe6e077d3e26865197d5a1452a29650f4
reyhanwidyatna <harwendareyhan@gmail.com>
2023-03-27 14:56:55 +0700

    fix responsive

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,12 @@ function App() {
 
   return (
     <div className="main">
-      <div id="home" className="body mx-auto py-8">
+      <div id="home" className="body mx-auto py-6 md:py-8">
         <div className="navbar flex justify-center items-center rounded-3xl">
           <div className="shadow-md text-gray-600 text-sm font-semibold rounded-3xl py-2 px-8">
             {navigation.map((item, index) => (
               <span
-                className="navbar-item text-lg cursor-pointer"
+                className="navbar-item text-sm md:text-lg cursor-pointer"
                 onClick={() => handleClick(item.id)}
                 key={index}
               >
@@ -36,34 +36,34 @@ function App() {
             ))}
           </div>
         </div>
-        <div className="background px-8 md:px-28 my-12 md:my-24">
+        <div className="background px-8 md:px-28 my-10 md:my-24">
           <p className="text-3xl md:text-5xl font-bold">
             Reyhan Widyatna Harwenda
           </p>
-          <p className="text-gray-600 text-md md:text-xl mt-8">
+          <p className="text-gray-600 text-md md:text-xl mt-4 md:mt-8">
             Frontend Engineer
           </p>
           <img className="background-image mt-24" src={home} alt="banner" />
         </div>
-        <div id="experience" className="experience p-12 md:p-20">
+        <div id="experience" className="experience p-8 md:p-20">
           <div className="experience-detail flex-row md:flex justify-between">
             <div className="projects">
-              <p className="font-semibold text-2xl">Projects</p>
+              <p className="font-semibold text-xl md:text-2xl">Projects</p>
               {projects.map((item, index) => (
                 <div className="projects-detail" key={index}>
-                  <p className="projects-date font-bold text-sm leading-4">
+                  <p className="projects-date font-bold text-xs md:text-sm leading-4">
                     {item.date}
                   </p>
-                  <p className="font-semibold mt-2">{item.title}</p>
-                  <p className="text-gray-500 text-md mt-2 mb-2 leading-6 text-justify">
+                  <p className="font-semibold text-sm md:text-md mt-2">{item.title}</p>
+                  <p className="text-gray-500 text-sm md:text-md mt-2 mb-2 leading-6 text-justify">
                     {item.description}
                   </p>
-                  <a className="projects-link text-md" href={item.visit} target="_blank" rel="noreferrer">{item.visit}</a>
+                  <a className="projects-link text-sm md:text-md" href={item.visit} target="_blank" rel="noreferrer">{item.visit}</a>
                 </div>
               ))}
             </div>
-            <div className="works border rounded-lg h-fit mt-12 px-4 md:mt-0">
-              <p className="font-semibold text-2xl">Work</p>
+            <div className="works border rounded-lg h-fit mt-12 md:px-4 md:mt-0">
+              <p className="font-semibold text-xl md:text-2xl">Work</p>
               {works.map((item, index) => (
                 <div className="works-detail flex items-center" key={index}>
                   <div className="company mr-4 border flex justify-center items-center">
@@ -74,12 +74,12 @@ function App() {
                     />
                   </div>
                   <div className="w-full">
-                    <p className="font-semibold text-md mb-1">
+                    <p className="font-semibold text-sm md:text-md mb-1">
                       {item.company}
                     </p>
                     <div className="flex justify-between">
-                      <p className="text-gray-500 text-sm">{item.title}</p>
-                      <p className="text-gray-500 text-sm">{item.date}</p>
+                      <p className="text-gray-500 text-xs md:text-sm mr-2">{item.title}</p>
+                      <p className="text-gray-500 text-xs md:text-sm">{item.date}</p>
                     </div>
                   </div>
                 </div>
@@ -98,41 +98,41 @@ function App() {
         </div>
         <div id="about" className="about p-8 md:p-20">
           <div className="about-detail flex flex-col items-center">
-            <p className="text-center text-white font-bold text-3xl mb-16">
+            <p className="text-center text-white font-bold text-xl md:text-3xl mb-8 md:mb-16">
               About me
             </p>
-            <p className="text-white text-lg text-justify mb-16">
+            <p className="text-white text-sm md:text-lg text-justify mb-10 md:mb-16">
               Hi there, I’m Reyhan and im a software engineer that currently focused on frontend
               engineering with almost 2 years experience. I also like to dive deep
               into and cover all areas of software engineering. I'm also curious
               and willing to try new things like product management. I was graduated from 
               IPB University in 2021. My hobbies are playing computer games and watching movies.
             </p>
-            <p className="text-white text-xl font-bold w-full mb-8">My Last Education</p>
-            <div className="flex items-center text-white w-full mb-16">
+            <p className="text-white text-md md:text-xl font-bold w-full mb-4 md:mb-8">My Last Education</p>
+            <div className="flex items-center text-white w-full mb-10 md:mb-16">
               <img
-                className="about-education mr-8"
+                className="about-education mr-4 md:mr-8"
                 src={ipb}
                 alt="education"
               />
               <div>
-                <p className="font-semibold text-lg">Bogor Agricultural University</p>
-                <p className="text-lg mb-4">
+                <p className="font-semibold text-sm md:text-lg">Bogor Agricultural University</p>
+                <p className="text-xs md:text-lg mb-4">
                   Computer Science, Faculty of Mathematics and Natural Science
                 </p>
-                <div className="flex text-md mt-2">
+                <div className="flex text-xs md:text-md mt-2">
                   <span className="mr-4">GPK: 3.32</span>
                   <span>2017 - 2021</span>
                 </div>
               </div>
             </div>
-            <p className="text-white text-xl font-bold w-full mb-8">Find Me On</p>
-            <div className="grid gap-6 grid-cols-3 mb-4">
-              <div className="flex flex-col justify-center items-center text-center text-white py-8 px-4 border-4 border-white rounded">
-                <div className="flex justify-center items-center about-social-media mb-10">
+            <p className="text-white text-md md:text-xl font-bold w-full mb-4 md:mb-8">Find Me On</p>
+            <div className="grid gap-6 md:grid-cols-3 mb-4">
+              <div className="text-sm md:text-md flex flex-col justify-center items-center text-center text-white py-4 px-2 md:py-8 md:px-4 border-4 border-white rounded">
+                <div className="flex justify-center items-center about-social-media mb-6 md:mb-10">
                   <img src={logoLinkedin} alt="linkedin" />
                 </div>
-                <p className="text-md mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
+                <p className="mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
                 <a
                   className="about-button py-1 px-4 font-semibold rounded"
                   href="https://www.linkedin.com/in/reyhan-widyatna-harwenda/"
@@ -141,11 +141,11 @@ function App() {
                   Visit
                 </a>
               </div>
-              <div className="flex flex-col justify-center items-center text-center text-white py-8 px-4 border-4 border-white rounded">
-                <div className="flex justify-center items-center about-social-media mb-10">
+              <div className="text-sm md:text-md flex flex-col justify-center items-center text-center text-white py-8 px-4 border-4 border-white rounded">
+                <div className="flex justify-center items-center about-social-media mb-6 md:mb-10">
                   <img src={logoInstagram} alt="instagram" />
                 </div>
-                <p className="text-md mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
+                <p className="mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
                 <a
                   className="about-button py-1 px-4 font-semibold rounded"
                   href="https://www.instagram.com/reyhannwh/"
@@ -154,11 +154,11 @@ function App() {
                   Visit
                 </a>
               </div>
-              <div className="flex flex-col justify-center items-center text-center text-white py-8 px-4 border-4 border-white rounded">
-                <div className="flex justify-center items-center about-social-media mb-10">
+              <div className="text-sm md:text-md flex flex-col justify-center items-center text-center text-white py-8 px-4 border-4 border-white rounded">
+                <div className="flex justify-center items-center about-social-media mb-6 md:mb-10">
                   <img src={logoGmail} alt="gmail" />
                 </div>
-                <p className="text-md mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
+                <p className="mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil molestiae amet. Sit unde dicta quaerat accusamus</p>
                 <a
                   className="about-button py-1 px-4 font-semibold rounded"
                   href="mailto:harwendareyhan@gmail.com?subject=subject&cc=cc@example.com"
@@ -170,8 +170,8 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="footer flex justify-between items-center border-t pt-6 md:pt-10 px-8 md:px-28">
-          <div className="flex text-lg text-gray-600 font-semibold">
+        <div className="footer flex justify-between items-center border-t pt-6 md:pt-8 px-8 md:px-28">
+          <div className="flex text-sm md:text-lg text-gray-600 font-semibold">
             {navigation.map((item, index) => (
               <span
                 className="navbar-item cursor-pointer"
@@ -182,7 +182,7 @@ function App() {
               </span>
             ))}
           </div>
-          <div className="flex items-center">
+          <div className="footer-social-media md:flex items-center">
             <a
               className="mr-6"
               href="https://www.linkedin.com/in/reyhan-widyatna-harwenda/"
