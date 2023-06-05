8cb2ef425b78713486c63921363c64f07bdaeb27
reyhanwidyatna <harwendareyhan@gmail.com>
2023-06-05 11:03:37 +0700

    change card behavior and add css variable

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,52 +6,73 @@ export default function Contact() {
   return (
     <div id="findme" className="findme p-8 md:p-20">
       <div className="findme-detail">
-        <p className="text-base md:text-xl font-bold w-full mb-6 md:mb-8">Find Me On</p>
-        <div className="grid gap-8 md:gap-20 md:grid-cols-3 mb-4">
-          <div className="flex flex-col justify-between items-center text-center text-sm md:text-base py-4 px-4 md:py-10 md:px-8 border-4 rounded">
-            <div className="flex justify-center items-center findme-social-media mb-6 md:mb-10">
-              <img src={logoLinkedin} alt="linkedin" />
+        <p className="text-base md:text-xl font-bold w-full mb-6 md:mb-8">Find Me</p>
+        <div className="grid gap-8 md:gap-32 md:grid-cols-3 mb-4">
+          <div className="findme-card">
+            <div className="findme-card-inner flex justify-center items-center text-center text-sm md:text-base">
+              <div className="findme-card-front flex flex-col justify-center items-center">
+                <div className="findme-social-media mb-6">
+                  <img src={logoLinkedin} alt="linkedin" />
+                </div>
+                <p className="text-base md:text-lg font-semibold">LinkedIn</p>
+              </div>
+              <div className="findme-card-back flex flex-col justify-center items-center text-center">
+                <p className="mb-6 px-10">
+                  Check me out on LinkedIn to see my portofolio and see what I've done professionally!
+                </p>
+                <a
+                  className="findme-card-back-button py-1 px-4 font-semibold rounded"
+                  href="https://www.linkedin.com/in/reyhan-widyatna-harwenda/"
+                  target="_blank" rel="noreferrer"
+                >
+                  Visit
+                </a>
+              </div>
             </div>
-            <p className="mb-6">
-              Check me out on LinkedIn to see my portofolio and see what I've done professionally!
-            </p>
-            <a
-              className="findme-button py-1 px-4 font-semibold rounded"
-              href="https://www.linkedin.com/in/reyhan-widyatna-harwenda/"
-              target="_blank" rel="noreferrer"
-            >
-              Visit
-            </a>
           </div>
-          <div className="flex flex-col justify-between items-center text-center text-sm md:text-base py-4 px-4 md:py-10 md:px-8 border-4 rounded">
-            <div className="flex justify-center items-center findme-social-media mb-6 md:mb-10">
-              <img src={logoInstagram} alt="instagram" />
+          <div className="findme-card">
+            <div className="findme-card-inner flex justify-center items-center text-center text-sm md:text-base">
+              <div className="findme-card-front flex flex-col justify-center items-center">
+                <div className="findme-social-media mb-6">
+                  <img src={logoInstagram} alt="instagram" />
+                </div>
+                <p className="text-base md:text-lg font-semibold">Instagram</p>
+              </div>
+              <div className="findme-card-back flex flex-col justify-center items-center text-center">
+                <p className="mb-6 px-10">
+                  Check me out on Instagram to see what I do on a daily basis and what I do besides work!
+                </p>
+                <a
+                  className="findme-card-back-button py-1 px-4 font-semibold rounded"
+                  href="https://www.instagram.com/reyhannwh/"
+                  target="_blank" rel="noreferrer"
+                >
+                  Visit
+                </a>
+              </div>
             </div>
-            <p className="mb-6">
-              Check me out on Instagram to see what I do on a daily basis and what I do besides work!
-            </p>
-            <a
-              className="findme-button py-1 px-4 font-semibold rounded"
-              href="https://www.instagram.com/reyhannwh/"
-              target="_blank" rel="noreferrer"
-            >
-              Visit
-            </a>
           </div>
-          <div className="flex flex-col justify-between items-center text-center text-sm md:text-base py-4 px-4 md:py-10 md:px-8 border-4 rounded">
-            <div className="flex justify-center items-center findme-social-media mb-6 md:mb-10">
-              <img src={logoGmail} alt="gmail" />
+          <div className="findme-card">
+            <div className="findme-card-inner flex justify-center items-center text-center text-sm md:text-base">
+              <div className="findme-card-front flex flex-col justify-center items-center">
+                <div className="findme-social-media mb-6">
+                  <img src={logoGmail} alt="gmail" />
+                </div>
+                <p className="text-base md:text-lg font-semibold">Gmail</p>
+              </div>
+              <div className="findme-card-back flex flex-col justify-center items-center text-center">
+                <p className="mb-6 px-10">
+                  Check me out on LinkedIn to see my portofolio and see what I've done professionally!
+                </p>
+                <a
+                  className="findme-card-back-button py-1 px-4 font-semibold rounded"
+                  href="mailto:harwendareyhan@gmail.com?subject=subject&cc=cc@example.com"
+                  target="_blank" rel="noreferrer"
+                >
+                  Visit
+                </a>
+              </div>
             </div>
-            <p className="mb-6">
-              Feel free to email me if you'd like to just chat or offer an opportunity
-            </p>
-            <a
-              className="findme-button py-1 px-4 font-semibold rounded"
-              href="mailto:harwendareyhan@gmail.com?subject=subject&cc=cc@example.com"
-              target="_blank" rel="noreferrer"
-            >
-              Visit
-            </a>
           </div>
         </div>
       </div>
